55a2f153aacc2fd884345d04081dc4733a8d8445
Emiel Doek <57521297+EmielDoek@users.noreply.github.com>
2025-03-13 13:34:40 +0000

    Approval Scale Complete

---
+++ scripts/Approval.gd
@@ -0,0 +1,25 @@
+extends Node
+
+var m_approval : int = 0
+var m_debugInt : int = 1
+@onready var h_slider: HSlider = $CanvasLayer/HSlider
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+@warning_ignore("unused_parameter")
+func _process(delta: float) -> void:
+	if m_approval == 100 : m_debugInt = -1
+	if m_approval == 0   : m_debugInt = 1
+	m_approval += m_debugInt
+	h_slider.value = m_approval
+	print(m_approval)
+	
+
+func _addApproval(n: int) -> void:
+	m_approval += n
+	
