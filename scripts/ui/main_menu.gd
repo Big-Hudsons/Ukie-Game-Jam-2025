b5d8716da64432dca15a73898cf183a377998cbe
Emiel Doek <57521297+EmielDoek@users.noreply.github.com>
2025-03-14 14:38:30 +0000

    main menu start game sound effect implemented

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,7 +1,10 @@
 extends Control
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
 func _on_start_game_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
+	audio_stream_player.playing = true
+	if audio_stream_player.finished:
+		get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
 
 func _on_settings_pressed() -> void:
 	pass # Replace with function body.
