d0846781572059b7f374281e2b2c3c35f899ac32
Cai Mills <CaiMills111@outlook.com>
2025-03-14 13:19:11 +0000

    Pause Menu Implementation Fr this time

--- scripts/ui/pause_menu.gd
+++ scripts/ui/pause_menu.gd
@@ -1,12 +1,26 @@
 extends Control
 
+var pauseMenu : bool = false
+
+func _ready():
+	self.visible = false
+
+func _process(delta):
+	if Input.is_action_just_pressed("Escape"):
+		_displayPause()
+		
+func _displayPause():
+	if (pauseMenu == true):
+		pauseMenu = false
+		self.visible = false
+		$"../DecisionMaking/GameUI/Approval Bar".visible = true
+	else: if (pauseMenu == false):
+		pauseMenu = true
+		self.visible = true
+		$"../DecisionMaking/GameUI/Approval Bar".visible = false
+
 func _on_resume_pressed() -> void:
-	get_tree().paused = false
+	_displayPause()
 
-
-func _on_settings_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_main_menu_pressed() -> void:
+func _on_resign_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/levels/main_menu.tscn")
